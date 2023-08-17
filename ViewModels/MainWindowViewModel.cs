6411145cd57ae4c1823e2982af4322e3e06f507f
tkgpoyo <tkgpoyo@gmail.com>
2023-08-17 13:27:28 +0900

    display startup page

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Im_Analyzer.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private ViewModelBase _activeContent;
+        public ViewModelBase ActiveContent
+        {
+            get => _activeContent;
+            set
+            {
+                _activeContent = value;
+                RaisePropertyChanged("ActiveContent");
+            }
+        }
+        public MainWindowViewModel()
+        {
+            ActiveContent = new StartUpPageViewModel();
+        }
+    }
+}
